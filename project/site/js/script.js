bfb192f7cd37a0025360134fb5afc316b2181fe0
Kristie <69997420+kphan27@users.noreply.github.com>
2020-11-22 20:39:07 -0500

    Add files via upload

--- project/site/js/script.js
+++ project/site/js/script.js
@@ -1,6 +1,24 @@
-const toggleButton = document.getElementsByClassName ('toggle-button')[0]
-const nav = document.getElementsByClassName ('nav')[0]
+const navSlide = () => {
+	const burger = document.querySelector('.burger');
+	const nav = document.querySelector('.nav-links');
+	const navLinks = document.querySelectorAll('.nav-links li');
 
-toggleButton.addEventListener ('click', () => {
-	nav.classList.toggle('active')
-})
+	burger.addEventListener('click', ()=>{
+		//toggle navigation
+		nav.classList.toggle ('nav-active');
+
+		//animation links
+		navLinks.forEach((link, index)=>{
+			if (link.style.animation) {
+				link.style.animation = ''
+			} else {
+				link.style.animation = `navLinkFade 0.5s ease forwards ${index/7 + 0.3}s`;
+			}
+		});
+
+		//burger animation
+		burger.classList.toggle('toggle');
+	});
+}
+
+navSlide();
